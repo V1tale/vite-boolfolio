59d837893e6e6f32539a8e4b6d3d48e5ae0b0b26
V1tale <elativ.c@gmail.com>
2023-01-23 15:16:25 +0100

    add projects

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,60 +1,32 @@
 <script>
 export default {
-    name: 'ProjectCard',
+    name: "AppCard",
     data() {
         return {
-            contentMaxLength: 200,
-            baseUrl: "http://127.0.0.1:8000",
+            maxContentLength: 100
         }
     },
     props: {
-        project: Object,
+        project: Object
     },
     computed: {
-        contentPreview() {
-            if (this.project.content && this.project.content.length > this.contentMaxLength) {
-                return this.project.content.substr(0, this.contentMaxLength) + '...';
+        descriptionPreview() {
+            if (this.project.content && this.project.content.length > this.maxContentLength) {
+                return this.project.content.substring(0, this.maxContentLength) + "...";
             }
             return this.project.content;
         }
-    },
-    methods: {
-        // truncateText(text) {
-        //     if (text && text.length > this.contentMaxLength) {
-        //         return text.substr(0, this.contentMaxLength) + '...';
-        //     }
-        //     return text;
-        // }
     }
 }
 </script>
 
 <template>
-    <div class="card mb-3">
-        <div class="card-body">
-            <div class="row justify-content-between">
-                <div class="col-4">
-                    <img v-if="project.image" class="w-100" :src="`${baseUrl}/storage/${project.image}`" alt="">
-                    <div v-else>
-                        Nessuna immagine
-                    </div>
-                </div>
-                <div class="col-7">
-                    <h3 class="card-title">{{ project.title }}</h3>
-                    <div class="d-flex justify-content-between">
-                        <p>
-                            <span class="text-primary" v-for="technology in project.technologies" :key="technology.id">#{{ technology.name }} </span>
-                        </p>
-                        <p class="text-end text-primary">{{ project.type ? project.type.name : 'Nessuna tipologia' }}
-                        </p>
-                    </div>
-                    <p class="card-text">
-                        {{ contentPreview }}
-                    </p>
-                    <a class="btn btn-dark" href="">Leggi</a>
-                </div>
-            </div>
-
+    <div class="card mb-4">
+        <!-- <img src="..." class="card-img-top" alt="..."> -->
+        <div class="card-body" style="height:350px">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ descriptionPreview }}</p>
+            <a href="#" class="btn btn-primary">Dettagli</a>
         </div>
     </div>
 </template>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -2,10 +2,10 @@
 import axios from 'axios';
 // import AppLoader from '../components/AppLoader.vue';
 import AppCard from '../components/AppCard.vue';
+import AppLoader from '../components/AppLoader.vue';
 import { store } from '../store';
 export default {
     name: "Projects",
-    components: { AppCard},
     data() {
         return {
             store,
@@ -16,30 +16,44 @@ export default {
             loading: false
         };
     },
+    components: { AppCard, AppLoader},
     created() {
         this.getProjects(1);
     },
     methods: {
-        getProjects(page) {
-            console.log(page);
-            const options = {
-                params: {
-                    page
-                }
-            }
+        getProjects() {
             this.loading = true;
-            axios.get(`${this.store.apiUrl}/api/projects`, options).then(resp => {
-                console.log(resp.data.results);
-                this.projects = resp.data.results.data;
-                this.currentPage = resp.data.results.page;
-                this.lastPage = resp.data.results.last_page;
-                this.totalProjects = resp.data.results.total;
-                this.loading = false;
+            axios.get(`${this.store.apiUrl}/api/projects`).then(resp => {
+                this.projects = resp.data.results;
+                this.loading = false; 
             });
-        }
+        },
     },
 }
 </script>
 <template>
-    <AppCard />
+    <div class="container">
+        <h2 class="text-center mb-3">I nostri progetti</h2>
+        <AppLoader v-if="loading" />
+        <div v-else class="row">
+            <p class="text-end"> {{ projects.length }} progetti trovati</p>
+            <div class="col-3" v-for="project in projects" :key="project.id">
+                <AppCard :project="project" />
+            </div>
+        </div>
+        <!-- pages navigation -->
+        <!-- <nav class="navigation d-flex justify-content-between">
+            <div>
+                page {{ currentPage }} in {{ lastPage }}
+            </div>
+            <div>
+                <a class="btn btn-success me-2" :class="currentPage === 1 ? 'disabled' : ''" href="" @click.prevent="getProjects(currentPage - 1)">Back</a>
+                    <a class="btn btn-success" :class="{ 'disabled': currentPage === lastPage }" href="" @click.prevent="getProjects(currentPage + 1)">Next</a>
+            </div>
+                        
+        </nav> -->
+    </div>
 </template>
+
+<style lang="scss">
+</style>
